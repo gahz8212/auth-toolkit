6b3ab28c90c105d39779e3fbc66ead20e0af3362
sunghyun kim <berserk.gahz@gmail.com>
2024-06-17 16:58:25 +0900

    하위아이템의 합으로 상위아이템 값 표현

--- src/containers/r-view/R_ViewComponent.tsx
+++ src/containers/r-view/R_ViewComponent.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 
 const R_ViewComponent = () => {
     const relations = [
-
-        { u: 3, l: 5, ip: 2, ep: 5 },
-        { u: 3, l: 6, ip: 0, ep: 5 },
-        { u: 6, l: 7, ip: 2, ep: 5 },
-        { u: 6, l: 8, ip: 4, ep: 5 },
-        { u: 6, l: 14, ip: 2, ep: 5 },
-        { u: 9, l: 13, ip: 1, ep: 5 },
-        { u: 6, l: 9, ip: 0, ep: 5 },
-        { u: 9, l: 10, ip: 3, ep: 5 },
-        // { u: 9, l: 12 ,ip:4,ep:5},
-        // { u: 5, l: 22,ip:2,ep:5 },
-        // { u: 5, l: 20 ,ip:3,ep:5},
-        // { u: 3, l: 4 ,ip:6,ep:5},
-        // { u: 9, l: 11,ip:3 ,ep:5},
-        // { u: 5, l: 21,ip:2,ep:5 },
+        { u: 3, l: 4, ip: 1, ep: 5 },
+        { u: 5, l: 20 ,ip:1,ep:5},
+        { u: 3, l: 5, ip: 1, ep: 5 },
+        { u: 5, l: 21, ip: 1, ep: 5 },
+        { u: 21, l: 22, ip: 1, ep: 5 },
+        { u: 21, l: 6, ip: 1, ep: 5 },
+        { u: 21, l: 7, ip: 1, ep: 5 },
+        { u: 3, l: 8, ip: 1, ep: 5 },
+        { u: 3, l: 13, ip: 1, ep: 5 },
+        { u: 13, l: 9, ip: 1, ep: 5 },
+        { u: 8, l: 14, ip: 1, ep: 5 },
+        { u: 7, l: 10, ip: 1, ep: 5 },
+        { u: 9, l: 12, ip: 1, ep: 5 },
+        { u: 9, l: 11, ip: 1, ep: 5 },
     ]
 
     let array: { id: number, top: number, left: number, im_price: number, ex_price: number }[] = [];
     let lastLeft = 0;
-
+    let history: number[] = []
+    let itemId = 0;
     const findChild = (id: number, top: number, left: number, im_price: number, ex_price: number) => {
         const children = relations.filter(rel => rel.u === id).map(rel => ({ lower: rel.l, im_price: rel.ip, ex_price: rel.ep }))
         if (lastLeft >= left) {
@@ -30,30 +30,32 @@ const R_ViewComponent = () => {
 
         const newItem = { id: id, top: top, left: left, im_price: im_price, ex_price: ex_price }
         array.push(newItem)
-
+        if (history.length > 0) {
+            history.forEach(his => array.forEach(arr => arr.id === his ? arr.im_price += newItem.im_price : 0))
+        }
         if (children.length === 0) {
-            lastLeft = left > lastLeft ? left : lastLeft;
+            lastLeft = left > lastLeft ? left + 50 : lastLeft;
             return;
         }
-
         for (let index = 0; index < children.length; index++) {
-            newItem.im_price += children[index].im_price;
-            // array.filter(arr => arr.id === id)[0].im_price = newItem.im_price
-            findChild(children[index].lower, top + 100, left + index * 60, children[index].im_price, children[index].ex_price)
+            if (children[index].lower === 5 || children[index].lower === 8 || children[index].lower === 13 || children[index].lower === 4) history = []
+            if (!history.includes(id)) { history.push(id) }
+ 
+            findChild(children[index].lower, top + 60, left + index * 60, children[index].im_price, children[index].ex_price)
         }
 
     }
     const createGraph = (id: number) => {
-
-        findChild(id, 0, 100, 0, 0);
+        itemId = id
+        findChild(id, 0, 60, 0, 0);
     }
     createGraph(3)
 
 
     return (
         <div style={{ top: '10rem', position: 'relative' }}>
-            {array.map(item => <div key={item.id} style={{ fontSize: '11px', position: 'absolute', top: item.top, left: item.left, border: '1px solid black', width: '50px', height: '50px', textAlign: 'center' }}>
-                <div>{item.id}</div><div>입고가{item.im_price}</div><div>출고가{item.ex_price}</div>
+            {array.map(item => <div key={item.id} style={{ fontSize: '12px', position: 'absolute', top: item.top, left: item.left, border: '1px solid black', width: '50px', height: '50px', textAlign: 'center' }}>
+                <div>{item.id}</div><div>입고가{item.im_price}</div>
             </div>)
             }
         </div >
